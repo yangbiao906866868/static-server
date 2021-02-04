31fcf7f9f5b8e00595f5290b4857d160869e1e55
yangbiao <906866868@qq.commkdir>
2021-02-05 02:22:03 +0800

    动态服务器：注册登录

--- server.js
+++ server.js
@@ -18,16 +18,91 @@ var server = http.createServer(function(request, response){
   var method = request.method
 
   /******** 从这里开始看，上面不要看 ************/
-
+  const session = JSON.parse(fs.readFileSync('./session.json').toString())
   console.log('有个帅哥发请求过来了！路径（带查询参数）为：'+pathWithQuery)
+  
+  
 
+  if (path === "/sign_in" && method === "POST") {
+    const userArray = JSON.parse(fs.readFileSync("./db/users.json"));
+    const array = [];
+    request.on("data", chunk => {
+      array.push(chunk);
+    });
+    request.on("end", () => {
+      const string = Buffer.concat(array).toString();
+      const obj = JSON.parse(string); // name password
+      const user = userArray.find(
+        user => user.name === obj.name && user.password === obj.password
+      );
+      if (user === undefined) {
+        response.statusCode = 400;
+        response.setHeader("Content-Type", "text/json; charset=utf-8");
+      }else{
+        response.statusCode = 200
+        const random = Math.random()
+        const session = JSON.parse(fs.readFileSync('./session.json').toString())
+        session[random] = {user_id:user.id}
+        fs.writeFileSync('./session.json',JSON.stringify(session))
+        response.setHeader('Set-Cookie',`session_id=${random};HttpOnly`)
+      }
+      response.end()
+    })
+  }else if(path ==='/home.html'){
+    //写不出来
+    const cookie = request.headers['cookie']
+    let sessionId
+    try{
+      sessionId = cookie.split(';').filter(s=>s.indexOf('session_id=')>=0)[0].split('=')[1]
+    }catch(error){}
+    
+    if(sessionId && session[sessionId]){
+      const userId = session[sessionId].user_id
+      const userArray = JSON.parse(fs.readFileSync("./db/users.json"));
+      const user = userArray.find(user=>user.id===userId)
+      const homeHtml = fs.readFileSync("./public/home.html").toString()
+      let string = ''
+      if(user){
+        string = homeHtml.replace('{{loginStatus}}','已登陆')
+        .replace('{{user.name}}',user.name)
+      }
+      response.write(string)
+    }else{
+      const homeHtml = fs.readFileSync("./public/home.html").toString()
+      const string = homeHtml.replace('{{loginStatus}}','未登陆')
+      .replace('{{user.name}}','')
+      response.write(string)
+    }
+      response.end()
+  }else if (path === "/register" && method === "POST") {
+    response.setHeader("Content-Type", "text/html; charset=utf-8");
+    const userArray = JSON.parse(fs.readFileSync("./db/users.json"));
+    const array = [];
+    request.on("data", chunk => {
+      array.push(chunk);
+    });
+    request.on("end", () => {
+      const string = Buffer.concat(array).toString();
+      const obj = JSON.parse(string);
+      const lastUser = userArray[userArray.length - 1];
+      const newUser = {
+        // id 为最后一个用户的 id + 1
+        id: lastUser ? lastUser.id + 1 : 1,
+        name: obj.name,
+        password: obj.password
+      };
+      userArray.push(newUser);
+      fs.writeFileSync("./db/users.json", JSON.stringify(userArray));
+      response.end()
+    });
+  }else{
     response.statusCode = 200
     //默认首页
     const filePath = path === '/' ? '/index.html' : path
     const index = filePath.lastIndexOf('.')
     //suffix 是后缀
     const suffix = filePath.substring(index)
-    const  fileTypes = {
+    const fileTypes = {
         '.html':'text/html',
         '.css':'text/css',
         '.js':'text/javascript',
@@ -44,7 +119,8 @@ var server = http.createServer(function(request, response){
     }
     response.write(content)
     response.end()
-  
+  }
+
   /******** 代码结束，下面不要看 ************/
 })
 
